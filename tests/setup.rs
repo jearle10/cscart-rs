3e0c6b064e8040936b8e5c7fa56d4cba49cb2918
Jian Earle <jianearle93@googlemail.com>
2023-02-12 12:06:44 +0000

    fix: Updated api credentials test to check for username and host. Added dotenv file for configuring locally

--- tests/setup.rs
+++ tests/setup.rs
@@ -1,6 +1,18 @@
+use dotenv::dotenv;
+
 #[test]
-fn api_key_exists(){
+fn api_credentials_exist(){
+    dotenv().ok(); // For local testing
     let api_key = std::env::var("CSCART_API_KEY")
         .expect("No api key found");
-    assert_eq!(api_key.len() > 0 , true)
+
+    let username = std::env::var("CSCART_USERNAME")
+        .expect("No username found");
+
+    let host = std::env::var("CSCART_HOST")
+        .expect("No host found");
+
+    assert_eq!(api_key.len() > 0 , true);
+    assert_eq!(username.len() > 0 , true);
+    assert_eq!(host.len() > 0 , true);
 }
