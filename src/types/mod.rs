377b6bf0773b2270ebd22c87169c265cbf59fdc2
Jian Earle <jianearle93@googlemail.com>
2024-03-18 13:51:56 +0000

    fix: Make Product fields public

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -4,75 +4,75 @@ use serde_json::Value;
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Product {
-    product: String,
-    category_ids: Vec<i32>,
+    pub product: String,
+    pub category_ids: Vec<i32>,
     // #[serde(deserialize_with = "deserialize_string_or_int")]
-    main_category: i32,
-    price: String,
+    pub main_category: i32,
+    pub price: String,
     #[serde(deserialize_with = "deserialize_string_or_int")]
-    company_id: i32,
-    status: String,
+    pub company_id: i32,
+    pub status: String,
     #[serde(deserialize_with = "deserialize_string_or_int")]
-    amount: i32,
-    avail_since: String,
+    pub amount: i32,
+    pub avail_since: String,
     // #[serde(deserialize_with = "deserialize_string_or_int")]
-    box_height: Option<i32>, // Only present for /products/<id>
+    pub box_height: Option<i32>, // Only present for /products/<id>
     #[serde(deserialize_with = "deserialize_string_or_int")]
-    length: i32,
+    pub length: i32,
     // #[serde(deserialize_with = "deserialize_string_or_int")]
-    box_width: Option<i32>, // Only present for /products/<id>
-    details_layout: String,
-    edp_shipping: String,
-    exceptions_type: String,
-    free_shipping: String,
-    full_description: Option<String>,
+    pub box_width: Option<i32>, // Only present for /products/<id>
+    pub details_layout: String,
+    pub edp_shipping: String,
+    pub exceptions_type: String,
+    pub free_shipping: String,
+    pub full_description: Option<String>,
     // image_pairs: //
-    is_dep: Option<String>,
-    lang_code: Option<String>,
+    pub is_dep: Option<String>,
+    pub lang_code: Option<String>,
     #[serde(deserialize_with = "deserialize_string_or_float")] // Update to float
-    list_price: f32,
-    list_qty_count: String,
-    localisation: Option<String>,
-    low_avail_limit: String,
+    pub list_price: f32,
+    pub list_qty_count: String,
+    pub localisation: Option<String>,
+    pub low_avail_limit: String,
     // main_pair
-    max_qty: String,
+    pub max_qty: String,
     // #[serde(deserialize_with = "deserialize_string_or_int")]
     // max_items_in_box: Option<i32>, // Only present for /products/<id>
-    meta_description: Option<String>,
-    meta_keywords: Option<String>,
+    pub meta_description: Option<String>,
+    pub meta_keywords: Option<String>,
     // #[serde(deserialize_with = "deserialize_string_or_int")]
     // min_items_in_box: Option<i32>, // Only present for /products/<id>
     #[serde(deserialize_with = "deserialize_string_or_int")]
-    min_qty: i32,
-    options_type: Option<String>,
-    out_of_stock_actions: String,
-    page_title: Option<String>,
-    point_price: Option<String>,
-    popularity: Option<String>,
-    product_code: String,
+    pub min_qty: i32,
+    pub options_type: Option<String>,
+    pub out_of_stock_actions: String,
+    pub page_title: Option<String>,
+    pub point_price: Option<String>,
+    pub popularity: Option<String>,
+    pub product_code: String,
     // product_features
     #[serde(deserialize_with = "deserialize_string_or_int")]
-    product_id: i32,
-    promo_text: Option<String>,
+    pub product_id: i32,
+    pub promo_text: Option<String>,
     #[serde(deserialize_with = "deserialize_string_or_int")]
-    qty_step: i32,
-    return_period: String,
-    sales_amount: Option<String>,
-    search_words: Option<String>,
-    seo_name: String,
-    shared_product: Option<String>,
-    shipping_freight: String,
-    shipping_params: String,
-    short_description: Option<String>,
+    pub qty_step: i32,
+    pub return_period: String,
+    pub sales_amount: Option<String>,
+    pub search_words: Option<String>,
+    pub seo_name: String,
+    pub shared_product: Option<String>,
+    pub shipping_freight: String,
+    pub shipping_params: String,
+    pub short_description: Option<String>,
     // tax_ids: Vec<String>, // Array or string
-    timestamp: String,
-    tracking: Option<String>,
-    unlimited_download: String,
-    updated_timestamp: String,
+    pub timestamp: String,
+    pub tracking: Option<String>,
+    pub unlimited_download: String,
+    pub updated_timestamp: String,
     // usergroup_ids: Vec<String>,
     #[serde(deserialize_with = "deserialize_string_or_float")]
-    weight: f32,
-    zero_price_action: String,
+    pub weight: f32,
+    pub zero_price_action: String,
 }
 
 fn deserialize_string_or_int<'de, D>(deserializer: D) -> Result<i32, D::Error>
