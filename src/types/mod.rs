f357740855faaff65892a81812c4cbabc63d48cc
Jian Earle <jianearle93@googlemail.com>
2024-05-15 22:09:56 +0100

    get and create offer methods

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -6,7 +6,6 @@ use serde_json::Value;
 pub struct Product {
     product: String,
     category_ids: Vec<i32>,
-    // #[serde(deserialize_with = "deserialize_string_or_int")]
     main_category: i32,
     price: String,
     #[serde(deserialize_with = "deserialize_string_or_int")]
@@ -15,33 +14,29 @@ pub struct Product {
     #[serde(deserialize_with = "deserialize_string_or_int")]
     amount: i32,
     avail_since: String,
-    // #[serde(deserialize_with = "deserialize_string_or_int")]
-    box_height: Option<i32>, // Only present for /products/<id>
+    box_height: Option<i32>,
     #[serde(deserialize_with = "deserialize_string_or_int")]
     length: i32,
-    // #[serde(deserialize_with = "deserialize_string_or_int")]
-    box_width: Option<i32>, // Only present for /products/<id>
+    box_width: Option<i32>,
     details_layout: String,
     edp_shipping: String,
     exceptions_type: String,
     free_shipping: String,
     full_description: Option<String>,
-    // image_pairs: //
+    //  TODO image_pairs
     is_dep: Option<String>,
     lang_code: Option<String>,
-    #[serde(deserialize_with = "deserialize_string_or_float")] // Update to float
+    #[serde(deserialize_with = "deserialize_string_or_float")]
     list_price: f32,
     list_qty_count: String,
     localisation: Option<String>,
     low_avail_limit: String,
-    // main_pair
+    // TODO main_pair
     max_qty: String,
-    // #[serde(deserialize_with = "deserialize_string_or_int")]
-    // max_items_in_box: Option<i32>, // Only present for /products/<id>
+    max_items_in_box: Option<i32>,
     meta_description: Option<String>,
     meta_keywords: Option<String>,
-    // #[serde(deserialize_with = "deserialize_string_or_int")]
-    // min_items_in_box: Option<i32>, // Only present for /products/<id>
+    min_items_in_box: Option<i32>,
     #[serde(deserialize_with = "deserialize_string_or_int")]
     min_qty: i32,
     options_type: Option<String>,
@@ -50,7 +45,7 @@ pub struct Product {
     point_price: Option<String>,
     popularity: Option<String>,
     product_code: String,
-    // product_features
+    // TODO product_features
     #[serde(deserialize_with = "deserialize_string_or_int")]
     product_id: i32,
     promo_text: Option<String>,
@@ -64,12 +59,14 @@ pub struct Product {
     shipping_freight: String,
     shipping_params: String,
     short_description: Option<String>,
-    // tax_ids: Vec<String>, // Array or string
+    #[serde(deserialize_with = "deserialize_string_or_int_to_vec_i32")]
+    tax_ids: Vec<i32>,
     timestamp: String,
     tracking: Option<String>,
     unlimited_download: String,
     updated_timestamp: String,
-    // usergroup_ids: Vec<String>,
+    #[serde(deserialize_with = "deserialize_string_or_int_to_vec_i32")]
+    usergroup_ids: Vec<i32>,
     #[serde(deserialize_with = "deserialize_string_or_float")]
     weight: f32,
     zero_price_action: String,
@@ -89,6 +86,47 @@ where
     match parse_result {
         Some(number) => Ok(number as i32),
         None => Err(serde::de::Error::custom("Could not parse")),
+    }
+}
+
+fn deserialize_string_or_int_to_vec_i32<'de, D>(deserializer: D) -> Result<Vec<i32>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let value = Value::deserialize(deserializer)?;
+
+    // Convert each variant to an array before parsing
+    let value_array = match value {
+        Value::Array(x) => x,
+        Value::String(y) => vec![Value::String(y)],
+        Value::Number(z) => vec![Value::Number(z)],
+        _ => vec![],
+    };
+
+    let mut parse_results: Vec<Option<i64>> = vec![];
+
+    for value in value_array {
+        match value {
+            Value::Array(values) => {
+                for i in values.iter() {
+                    match i {
+                        Value::String(a) => parse_results.push(a.parse::<i64>().ok()),
+                        Value::Number(b) => parse_results.push(b.as_i64()),
+                        _ => {}
+                    }
+                }
+            }
+            Value::String(string) => parse_results.push(string.parse::<i64>().ok()),
+            Value::Number(number) => parse_results.push(number.as_i64()),
+            _ => {}
+        }
+    }
+
+    match parse_results {
+        bad_result if bad_result.contains(&None) => {
+            Err(serde::de::Error::custom("Could not parse"))
+        }
+        mut good_result => Ok(good_result.iter_mut().map(|x| x.unwrap() as i32).collect()),
     }
 }
 
